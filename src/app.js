528eb0a3de5fc87c0be44878b70f5ffffe8817cb
felipecarpes <carpesz94@gmail.com>
2020-06-30 00:39:27 -0300

    add repositories functionalities and make tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const respository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(respository);
+
+  return response.json(respository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const findRepo = repositories.findIndex(repo => repo.id === id);
+
+  if (findRepo === -1) {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  };
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findRepo].likes
+  };
+
+  repositories[findRepo] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepo = repositories.findIndex(repo => repo.id === id);
+
+  if (findRepo >= 0) {
+    repositories.splice(findRepo, 1);
+  } else {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepo = repositories.find(repo => repo.id === id);
+
+  if (!findRepo) {
+    return response.status(400).json({ error: 'Repository does not exists.' })
+  }
+
+  findRepo.likes += 1;
+
+  return response.json(findRepo);
 });
 
 module.exports = app;
